99d2949de40e12d3b0ff8dcbd6827ddf6bffac0e
duexcoast <conor.ux@gmail.com>
2022-12-13 14:35:08 -0500

    passing test with all data

--- renderer.go
+++ renderer.go
@@ -1,6 +1,9 @@
 package blogrenderer
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Post struct {
 	Title, Description, Body string
@@ -8,5 +11,27 @@ type Post struct {
 }
 
 func Render(w io.Writer, p Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprint(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err := fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprint(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
