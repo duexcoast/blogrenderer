5fd7d99b533b0492fa6340e2814ea3bb164276b1
duexcoast <conor.ux@gmail.com>
2022-12-22 14:08:44 -0500

    refactored to use approval testing. index template passing

--- renderer_test.go
+++ renderer_test.go
@@ -2,13 +2,14 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
 	"github.com/duexcoast/blogrenderer"
 )
 
-func TestRenderTestRender(t *testing.T) {
+func TestRender(t *testing.T) {
 	var aPost = blogrenderer.Post{
 		Title:       "hello world",
 		Body:        "This is a post",
@@ -16,9 +17,14 @@ func TestRenderTestRender(t *testing.T) {
 		Tags:        []string{"go", "tdd"},
 	}
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
+		err := postRenderer.Render(&buf, aPost)
 
 		if err != nil {
 			t.Fatal(err)
@@ -27,4 +33,36 @@ func TestRenderTestRender(t *testing.T) {
 		approvals.VerifyString(t, buf.String())
 	})
 
+	t.Run("it renders an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogrenderer.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+
+}
+
+func BenchmarkRenderer(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
